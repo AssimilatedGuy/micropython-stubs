6c3cd32a9db8bf52df8fd9b2ae49dcd404d0fc25
Jos Verlinde <josverl@microsoft.com>
2021-05-16 02:08:18 +0200

    updates  stubs using stubgen - 6

--- stubs/micropython_cpython_core/uctypes.pyi
+++ stubs/micropython_cpython_core/uctypes.pyi
@@ -1,6 +1,3 @@
+from typing import Any
 
-from typing import Any, Dict, Optional, Sequence, Tuple, Union
-Node = Any
 def bytearray_at(addr: Any, sz: Any) -> Any: ...
-    #   0: return ctypes.string_at(addr,sz)
-    # ? 0: return ctypes.string_at(addr, sz)
