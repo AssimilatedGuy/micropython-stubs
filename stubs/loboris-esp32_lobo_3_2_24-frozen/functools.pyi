43598b17a9c03854ef8841f624fcd69f4bf2865e
Jos Verlinde <josverl@microsoft.com>
2021-05-16 02:04:15 +0200

    updates  stubs using stubgen - 3

--- stubs/loboris-esp32_lobo_3_2_24-frozen/functools.pyi
+++ stubs/loboris-esp32_lobo_3_2_24-frozen/functools.pyi
@@ -1,20 +1,6 @@
+from typing import Any, Optional
 
-from typing import Any, Dict, Optional, Sequence, Tuple, Union
-Node = Any
-def partial(func: Any, *args, **kwargs) -> Any:
-    #   0: return func(*args+more_args,None=kw)
-    # ? 0: return func(*args+more_args, None=kw)
-    #   1: return _partial
-    # ? 1: return _partial
-    def _partial(*more_args, **more_kwargs) -> Any: ...
-        #   0: return func(*args+more_args,None=kw)
-        # ? 0: return func(*args+more_args, None=kw)
+def partial(func: Any, *args: Any, **kwargs: Any) -> Any: ...
 def update_wrapper(wrapper: Any, wrapped: Any) -> Any: ...
-    #   0: return wrapper
-    # ? 0: return wrapper
 def wraps(wrapped: Any) -> Any: ...
-    #   0: return lambda x: x
-    # ? 0: return lambda x: x
-def reduce(function: Any, iterable: Any, initializer: Any=) -> Any: ...
-    #   0: return value
-    # ? 0: return value
+def reduce(function: Any, iterable: Any, initializer: Optional[Any]=...) -> Any: ...
